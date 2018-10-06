9a98d4331da11c88ae6e033f13810af7f86441d6
jccg891113 <sachuan@foxmail.com>
2018-10-06 22:45:26 +0800

    2018-10-6-23-45

--- src/com/Jack/Main.java
+++ src/com/Jack/Main.java
@@ -1,9 +1,21 @@
 package com.Jack;
 
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         System.out.println("Hello World!");
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("What's your name? ");
+        String name = in.nextLine();
+
+        System.out.print("How old are you? ");
+        int age = in.nextInt();
+
+        System.out.println("Hello, " + name + ". Next year, you'll be " + (age + 1));
     }
 }
